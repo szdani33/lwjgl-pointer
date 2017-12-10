d4412142132e30be0b0aafb06ea243430e36626a
szdani33 <daniel.tamas.szabo@gmail.com>
2017-12-10 17:01:33 +0100

    rectangle model added

--- src/main/java/hu/dtms/lwjgl/pointer/MainLoop.java
+++ src/main/java/hu/dtms/lwjgl/pointer/MainLoop.java
@@ -3,13 +3,25 @@ package hu.dtms.lwjgl.pointer;
 import org.lwjgl.opengl.Display;
 
 public class MainLoop {
-    private static final float[] VERTICES = {
+
+    private static final float[] RECTANGLE_VERTICES = {
+            -.2f, .2f,
+            -.2f, 0,
+            0, 0,
+            0, .2f
+    };
+    private static final int[] RECTANGLE_INDICES = {
+            0, 1, 2,
+            0, 2, 3
+    };
+
+    private static final float[] POINTER_VERTICES = {
             0, 0,
             0.028f, -0.092f,
             0.04f, -0.068f,
             0.066f , -0.068f
     };
-    private static final int[] INDICES = {
+    private static final int[] POINTER_INDICES = {
             0, 1, 2,
             0, 2, 3
     };
@@ -17,16 +29,19 @@ public class MainLoop {
     private static Renderer renderer = new Renderer();
     private static Loader loader = new Loader();
     private static ShaderProgram shaderProgram;
-    private static SimpleModel simpleModel;
+    private static SimpleModel pointerModel;
+    private static SimpleModel rectangleModel;
 
     public static void main(String[] args) {
         DisplayManager.createDisplay();
         shaderProgram = new ShaderProgram();
-        simpleModel = loader.loadSimpleModel(VERTICES, INDICES);
+        pointerModel = loader.loadSimpleModel(POINTER_VERTICES, POINTER_INDICES);
+        rectangleModel = loader.loadSimpleModel(RECTANGLE_VERTICES, RECTANGLE_INDICES);
         while (!Display.isCloseRequested()) {
             renderer.prepare();
             shaderProgram.start();
-            renderer.render(simpleModel);
+            renderer.render(rectangleModel);
+            renderer.render(pointerModel);
             shaderProgram.stop();
             DisplayManager.updateDisplay();
         }
